bb3be7e059d00438159d162441364e36b47b2749
Andrew Song <snappysong@gmail.com>
2021-08-20 16:11:54 -0400

    initial deployment on vercel

---
+++ build/precache-manifest.6b11d88432a34135137e0b357dea82c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20663929bae423a4f3a70cee94c8740c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f333588b4bb38d8bdb7",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f333588b4bb38d8bdb7",
+    "url": "/static/js/main.1ae2960c.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
